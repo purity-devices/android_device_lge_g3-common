de3c928b26e1794c75a549536bf1166f5a5c06b2
Daniel Sikorski <dadi.sikorski@gmail.com>
2016-03-20 11:49:52 +0300

    g3: fix proximity sensor
    Change-Id: I26fa1ffee891d70167c2238170babc595c2929e3

--- system_prop.mk
+++ system_prop.mk
@@ -63,4 +63,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
-    debug.qualcomm.sns.libsensor1=e
+    debug.qualcomm.sns.libsensor1=e \
+    gsm.proximity.enable=true
